4f1bcf2adea5a6270b5c31639e6962bb5dad96f4
ZeroVitz <orlando112000@gmail.com>
2016-10-14 00:44:07 -0600

    This is the final version!!!!

---
+++ app/src/main/java/crosscorporation/presentation/CompanyActivity.java
@@ -0,0 +1,13 @@
+package crosscorporation.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompanyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company);
+    }
+}
--- app/src/main/java/crosscorporation/presentation/MainActivity.java
+++ app/src/main/java/crosscorporation/presentation/MainActivity.java
@@ -1,13 +1,34 @@
 package crosscorporation.presentation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.button)
+        {
+            Intent intent = new Intent(this, CompanyActivity.class);
+            startActivity(intent);
+        }
     }
 }
